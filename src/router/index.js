1a4dca786e47f042bffd594a20674e6c82908782
houyaxin <724509494@qq.com>
2021-12-04 23:54:56 +0800

    完成订单功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -10,55 +10,27 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
+import Order from '../components/order/Order.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/login'
-  },
-  {
-    path: '/login',
-    component: Login
-  },
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
   {
     path: '/home',
     component: Home,
     redirect: '/welcome',
     children: [
-      {
-        path: '/welcome',
-        component: Welcome
-      },
-      {
-        path: '/users',
-        component: Users
-      },
-      {
-        path: '/rights',
-        component: Rights
-      },
-      {
-        path: '/roles',
-        component: Roles
-      },
-      {
-        path: '/categories',
-        component: Cate
-      },
-      {
-        path: '/params',
-        component: Params
-      },
-      {
-        path: '/goods',
-        component: List
-      },
-      {
-        path: '/goods/add',
-        component: Add
-      }
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles },
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params },
+      { path: '/goods', component: List },
+      { path: '/goods/add', component: Add },
+      { path: '/orders', component: Order }
     ]
   }
 ]
